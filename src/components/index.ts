052507d521df598e4b5be3bafddee18fe8835871
Progressive Dev <ProgressiveDev@Ilyas-MacBook-Pro.local>
2020-10-09 17:42:02 +0400

    Prepare for initial release

---
+++ src/components/index.ts
@@ -0,0 +1,15 @@
+export { default as Button } from './button';
+
+export { default as FormControl } from './form-control';
+
+export { default as Link } from './link';
+
+export { default as Search } from './search';
+
+export { default as Select } from './select';
+
+export { default as Skeleton } from './skeleton';
+
+export { default as Spinner } from './spinner';
+
+export { default as TextInput } from './text-input';
