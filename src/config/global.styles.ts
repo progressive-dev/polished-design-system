836f2893575cfe5698b6254906a23f3e6b95c279
Progressive Dev <ProgressiveDev@Ilyas-MacBook-Pro.local>
2020-10-09 19:08:04 +0400

    Change the way to load fonts

--- src/config/global.styles.ts
+++ src/config/global.styles.ts
@@ -1,10 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
-import './fonts/Poppins-Bold.ttf';
-import './fonts/Poppins-Light.ttf';
-import './fonts/Poppins-Medium.ttf';
-import './fonts/Poppins-Regular.ttf';
-import './fonts/Poppins-SemiBold.ttf';
-import './fonts/Poppins-Thin.ttf';
+import './fonts/styles.css';
 
 export const GlobalStyles = createGlobalStyle`
   * {
