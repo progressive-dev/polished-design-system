7c0048ffb960bbbca0597a8925abc665e239d78d
Progressive Dev <ProgressiveDev@Ilyas-MacBook-Pro.local>
2020-10-09 18:52:57 +0400

    Include fonts into the bundle

--- src/config/global.styles.ts
+++ src/config/global.styles.ts
@@ -1,7 +1,12 @@
-
+import './fonts/Poppins-Bold.ttf';
+import './fonts/Poppins-Light.ttf';
+import './fonts/Poppins-Medium.ttf';
+import './fonts/Poppins-Regular.ttf';
+import './fonts/Poppins-SemiBold.ttf';
+import './fonts/Poppins-Thin.ttf';
 import { createGlobalStyle } from 'styled-components';
 
-export const GlobalStyle = createGlobalStyle`
+export const GlobalStyles = createGlobalStyle`
   * {
     font-family: 'Poppins';
     box-sizing: border-box;
